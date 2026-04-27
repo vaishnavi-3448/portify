47696d7e8d73df3bb91c599440383299688c08dc
vaishnavi-3448 <vaishnavig1914@gmail.com>
2026-04-28 01:18:23 +0530

    Update API endpoint for resume data retrieval

--- frontend/src/pages/PortfolioPage.jsx
+++ frontend/src/pages/PortfolioPage.jsx
@@ -8,7 +8,7 @@ function PortfolioPage() {
 
   useEffect(() => {
     axios
-      .get(`http://127.0.0.1:5000/resume/${id}`)
+      .get(`http://98.93.120.138:5000/resume/${id}`)
       .then((res) => setData(res.data.parsed_data))
       .catch((err) => console.error(err));
   }, [id]);
